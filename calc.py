b65c23711029c006bb65be13af345dc2db9a7c42
lukeprograming <82295147+lukeprograming@users.noreply.github.com>
2021-10-23 21:49:01 -0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,14 @@
+class Calc:
+  def soma(* num):
+    for i in num:
+      a = a + num[i]
+    return a
+
+print("Bem Vindo")
+print("""---------------------------
+Esse aplicativo é uma calculadora simples
+com funções basicas de matematica como a 
+soma, multiplicação, divisão e subtração
+com o passa do tempo deve have mais funções
+como potencia, raiz quadrada, bascara, etc""")
+
